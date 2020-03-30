ad1d2ef5b77bed707f96434be1773350331e0ba4
Kenneth Ybanez <kybanez53@gmail.com>
2020-03-31 00:07:20 +0800

    added database

--- microblogdb (1).sql
+++ microblogdb (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 26, 2020 at 06:30 PM
+-- Generation Time: Mar 30, 2020 at 06:06 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.3
 
@@ -108,7 +108,7 @@ CREATE TABLE `users` (
   `code` varchar(255) DEFAULT NULL,
   `activation_status` tinyint(1) NOT NULL DEFAULT 0,
   `created` timestamp NULL DEFAULT current_timestamp(),
-  `modified` timestamp NULL DEFAULT NULL,
+  `modified` timestamp NULL DEFAULT current_timestamp(),
   `deleted_date` timestamp NULL DEFAULT NULL,
   `deleted` tinyint(1) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -118,8 +118,17 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `email`, `first_name`, `middle_name`, `last_name`, `date_of_birth`, `image`, `code`, `activation_status`, `created`, `modified`, `deleted_date`, `deleted`) VALUES
-(115, 'd', '$2a$10$iEKRstUBceGINXbUCARmQ.LetvhwEPx62UOXDf1loapw30ekpVSLO', 'kybanez53@gmail.com', 'kenneth', 'baldado', 'ybanez', '1997-07-13', 'user.png', 'MB-3393', 0, '2020-03-26 17:25:02', '2020-03-26 17:25:02', NULL, 1),
-(117, 'hy', '$2a$10$oKWGTCdUe0zaMxpPXwtMb.GnDuAqxL8tR5zH8ywhw03NBO.3opv5C', 'kybanez53@gmail.com', 'kenneth', 'baldado', 'ybanez', '1997-07-13', 'user.png', 'MB-1253', 0, '2020-03-26 17:28:15', '2020-03-26 17:28:15', NULL, 1);
+(171, 'hyper', '$2a$10$.WoUK2iM.oOyU.14FHllS.tJQM0Vpp0YJVha35Ggmtah/hf1H1VdS', 'kennethybanez.yns@gmail.com', 'kenneth', 'baldado', 'ybanez', '1997-07-13', 'user.png', 'MB-5704', 1, '2020-03-28 15:15:37', '2020-03-30 07:09:12', NULL, 1),
+(199, 'popo', '$2a$10$87PAYVWqWW1FhaD5JXBJX.aOSpnM4npjucQ82ll8eNtwBh5Sh1RiO', 'kennethybanez.yns@gmail.com', 'pogi', 'pogi', 'pogi', '1892-02-02', 'user.png', 'MB-7727', 1, '2020-03-30 01:44:48', '2020-03-30 07:24:18', NULL, 1),
+(200, 'adi94fdfgrsdfsdfdsdfgt8fssdssd', '$2a$10$Nm.Luj8kovgpZSjfxcpqLOv9Dua2ZqCpMG1w.T4etggvtHfELFfnG', 'kennethybanez.yns@gmail.com', 'kenneth', 'baldado', 'ybanez', '1997-07-13', 'user.png', 'MB-7316', 0, '2020-03-30 03:09:12', '2020-03-30 03:09:12', NULL, 1),
+(201, 'adi9t4fdfgrsdfsdfdsdfgt8fssdssd', '$2a$10$5X8zqg9NclyguodsNv5KveJECRn8gjYa7KbzOPz5riy2jEIyik4WK', 'kennethybanez.yns@gmail.com', 'kenneth', 'baldado', 'ybanez', '1997-07-13', 'user.png', 'MB-2141', 0, '2020-03-30 03:12:18', '2020-03-30 03:12:18', NULL, 1),
+(202, 'adi9t4fdfgssrsdfsdfdsdfgt8fssdssd', '$2a$10$1OqxlwkvHZcenF69AK74Keu7rhDVC/ibYYmRJc0R2pg1mZ4rSBOT6', 'kennethybanez.yns@gmail.com', 'kenneth', 'baldado', 'ybanez', '1997-07-13', 'user.png', 'MB-1879', 0, '2020-03-30 03:13:37', '2020-03-30 03:13:37', NULL, 1),
+(203, 'a', '$2a$10$4djlHs5d6C17y2isKmdvgugAMwn9uqjjtMlj65FTs/A4BTNl0h.z2', 'kennethybanez.yns@gmail.com', 'kenneth', 'baldado', 'ybanez', '1997-07-13', 'user.png', 'MB-7494', 0, '2020-03-30 03:26:35', '2020-03-30 03:26:35', NULL, 1),
+(204, 'ba', '$2a$10$cYTcHceZCptiGrTXMWY1uuaQjjSklZ9rtkHOuW3boop36JEd7sp1O', 'kennethybanez.yns@gmail.com', 'kenneth', 'baldado', 'ybanez', '1997-07-13', 'user.png', 'MB-7358', 0, '2020-03-30 03:26:58', '2020-03-30 03:26:58', NULL, 1),
+(205, 'bda', '$2a$10$DYS6nomIEglRE6M7fhzVVeiJFxJGS0lVUKViPekNcUvGGikuZwJEG', 'kennethybanez.yns@gmail.com', 'kenneth', 'baldado', 'ybanez', '1997-07-13', 'user.png', 'MB-9963', 0, '2020-03-30 03:35:34', '2020-03-30 03:35:34', NULL, 1),
+(206, 'bdsa', '$2a$10$nC2VCFIfOHkqsshsK.wBuuQIhSEtFqMlsYsk2wdXmOop9g//a8Ble', 'kennethybanez.yns@gmail.com', 'kenneth', 'baldado', 'ybanez', '1997-07-13', 'user.png', 'MB-7798488', 1, '2020-03-30 04:02:49', '2020-03-30 04:02:49', NULL, 1),
+(207, 'dbdsa', '$2a$10$bgta/qBmGCcwUQNgeuNYEOmfEn9uM7bqOH6lDLqJm3NMMkt/sQN.a', 'kennethybanez.yns@gmail.com', 'kenneth', 'baldado', 'ybanez', '1997-07-13', 'user.png', 'MB-8596410', 1, '2020-03-30 04:05:10', '2020-03-30 04:05:10', NULL, 1),
+(208, 'popopo', '$2a$10$RcK0nIeBQRasLE.aXMkhsevXR7HNTOReU4W2/NS2YlmmlAUJ/dXxy', 'kennethybanez.yns@gmail.com', 'popo', 'popo', 'popo', '2020-04-02', 'user.png', 'MB-21659311', 0, '2020-03-30 14:35:08', '2020-03-30 14:35:08', NULL, 1);
 
 --
 -- Indexes for dumped tables
@@ -187,7 +196,7 @@ ALTER TABLE `posts`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=118;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=209;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
